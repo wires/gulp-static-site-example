e2024120a22c915cdcdb5c0ebfa4a9a5f79b0819
wires <jelle@defekt.nl>
2016-02-22 04:52:35 +0100

    ditched some broken libs
    - gulp-ruby-sass => `gulp-sass`
    - gulp-load-plugins => nope, be explicit is better
    - gulp-marked => broken, replaced by `gulp-markdown`
    - gulp-clean => replace by `gulp-rimraf`

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,41 @@
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
+var gulp = require('gulp')
+
+var $ = {
+  rimraf: require('gulp-rimraf'),
+  markdown: require('gulp-markdown'),
+  sass: require('gulp-sass'),
+  size: require('gulp-size'),
+  staticSite: require('gulp-static-site')
+}
 
 // convert our markdown to html
 gulp.task('markdown', function () {
-    return gulp.src('*.md')
-        .pipe($.marked())
-        .pipe(gulp.dest('pre-build/'))
-});
+  return gulp.src('content/*.md')
+    .pipe($.markdown())
+    .pipe(gulp.dest('pre-build/'))
+})
 
 // include all HTML files from prebuild into our site menu
 gulp.task('index', function () {
-    return gulp.src('pre-build/*.html')
-        .pipe($.staticSite('template.jade'))
-        .pipe(gulp.dest('build/'))
-        .pipe($.size());
-});
+  return gulp.src('pre-build/*.html')
+    .pipe($.staticSite('template.jade'))
+    .pipe(gulp.dest('build/'))
+    .pipe($.size())
+})
 
 // process our stylesheet
-gulp.task('sass', function(){
-    return gulp.src('style.scss')
-        .pipe($.rubySass())
-        .pipe(gulp.dest('build/css/'));
-});
+gulp.task('sass', function () {
+  return gulp.src('style.scss')
+    .pipe($.sass({outputStyle: 'compressed'}).on('error', $.sass.logError))
+    .pipe(gulp.dest('build/css/'))
+})
 
 // cleanup
 gulp.task('clean', function () {
-    return gulp.src(['build/', 'pre-build/'], {read: false})
-        .pipe($.clean());
-});
+  return gulp.src(['build/**/*', 'pre-build/**/*'], {read: false})
+    .pipe($.rimraf())
+})
 
 gulp.task('default', ['clean', 'sass', 'markdown'], function () {
-        gulp.start('index');
-});
+  gulp.start('index')
+})
